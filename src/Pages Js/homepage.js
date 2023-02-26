d69e6048aad5e27b3fa1577d65690d94d7fe8e28
KaviPrakash-p <119106375+KaviPrakash-p@users.noreply.github.com>
2023-02-26 22:33:39 +0530

    Add files via upload

---
+++ src/Pages Js/homepage.js
@@ -0,0 +1,16 @@
+import '../Pages Css/homepage.css'
+import Login from './Login';
+
+function Homepage() {
+    return(
+        <>
+        <div className="homeBody">
+            </div>
+            <div className="homepageBody">
+                 <a href='login'>LOG OUT</a>
+            </div>
+        </>
+    );
+}
+
+export default Homepage;
